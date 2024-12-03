c8f6f4f428e8d66d8170b26b54c5c2833878567e
MoseskingRT <moseswanyama261@gmail.com>
2024-12-03 20:10:14 +0300

    Update script.js

--- HTMLPROJECT/script.js
+++ HTMLPROJECT/script.js
@@ -1,7 +1,9 @@
-var typed = new Typed(".multiple-text",{
-	strings:["2D/3D Artist", "Emote Artist", "Twitch Streamer"],
-	typeSpeed: 100,
-	backSpeed: 100,
-	backDelay: 1000,
-	loop: true
-})
+document.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed(".multiple-text", {
+        strings: ["2D/3D Artist", "Emote Artist", "Twitch Streamer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true,
+    });
+});
